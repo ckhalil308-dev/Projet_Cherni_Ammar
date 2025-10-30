e5bdf9d8297450c9df1412e7c6a514c1d19bf5b1
Tasnim Ammar <tasnimammar2005@gmail.com>
2025-10-30 19:55:45 +0100

    routerlink progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { SiteList } from './components/site-list/site-list';
+import { Contact } from './components/contact/contact';
+import { Error } from './components/error/error';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', title:'Accueil', component:Home},
+    {path:'list', title:'Liste', component:SiteList},
+    {path:'contact', title:'Contact', component:Contact},
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'**', title:'Error', component:Error},
+];
---
+++ src/app/components/site-list/site-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-list',
+  imports: [],
+  templateUrl: './site-list.html',
+  styleUrl: './site-list.css',
+})
+export class SiteList {
+
+}

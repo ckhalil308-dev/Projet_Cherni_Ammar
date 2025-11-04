ac26dc2f4cbf63a60fe5ebf1718eb9d4fc1f0932
khalil <ckhalil308@gmail.com>
2025-11-04 09:58:10 +0100

    css progress

---
+++ src/app/component/sitesselected/sitesselected.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sitesselected',
+  imports: [],
+  templateUrl: './sitesselected.html',
+  styleUrl: './sitesselected.css',
+})
+export class Sitesselected {
+
+}

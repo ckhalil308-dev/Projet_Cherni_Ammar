2247b7d13a8bde008f4beba06171ad526513a070
Tasnim Ammar <tasnimammar2005@gmail.com>
2025-11-09 12:42:22 +0100

    commentaires admin list

--- src/app/components/admin/admin-comments/admin-comments.ts
+++ src/app/components/admin/admin-comments/admin-comments.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SitesService } from '../../../services/sites-service';
+import { Commentaires } from '../../../model/commentaires';
+import { Sites } from '../../../model/sites';
 
 @Component({
   selector: 'app-admin-comments',
@@ -6,6 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './admin-comments.html',
   styleUrl: './admin-comments.css',
 })
-export class AdminComments {
+export class AdminComments implements OnInit{
+  siteservice:SitesService=inject(SitesService);
+  siteList:Sites[]=[];
 
+  ngOnInit(): void {
+      this.siteservice.getSites().subscribe(
+        data=>this.siteList=data
+      )
+  }
 }

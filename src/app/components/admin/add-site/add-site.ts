4984a8d0fc05dcfcb110b43c9053bc00c8ef90f6
Tasnim Ammar <tasnimammar2005@gmail.com>
2025-11-15 11:47:19 +0100

    work on the add form

--- src/app/components/admin/add-site/add-site.ts
+++ src/app/components/admin/add-site/add-site.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-site',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './add-site.html',
   styleUrl: './add-site.css',
 })
-export class AddSite {
+export class AddSite implements OnInit{
+  siteForm!: FormGroup;
+  readonly formBuilder : FormBuilder = inject(FormBuilder);
+
+  ngOnInit(){
+    this.siteForm = this.formBuilder.nonNullable.group({
+      title : [],
+      era : [],
+      address : [],
+      price : [],
+      creation_date : [],
+      rating : [],
+      openingHours : [],
+      visitorsPerYear : [],
+      description : [],
+      thumbnail : [],
+      gallery : [],
+      open : []
+    })
+    
+  }
+
+  onSubmit() {
+    console.warn(this.siteForm.value);
+  }
 
 }
